2b1a54d89876c4da180888a08c68feeabc0be72f
Esmit Pérez <esmitperez@gmail.com>
2022-12-03 13:24:49 -0800

    Reorg code to share common code across days

--- src/day1.rs
+++ src/day1.rs
@@ -1,12 +1,6 @@
-use std::fs;
 use std::collections::HashMap;
 use std::collections::BinaryHeap;
 
-pub fn read(file_path: String) -> String {
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-    contents
-}
 
 pub fn parse(contents: String) -> HashMap<String, u32> {
     let mut scores = HashMap::new();
@@ -74,12 +68,6 @@ pub fn top_three(elves: &HashMap<String, u32>) -> u32 {
 mod tests {
     use super::*;
     const EXPECTED:&str = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
-
-    #[test]
-    fn read_works() {
-        let result = read("data/1/tests/input.txt".to_owned());
-        assert_eq!(result, EXPECTED);
-    }
 
     #[test]
     fn parse_works() {
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 mod day1;
-
-use std::env;
+mod day2;
+mod lib;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
     // dbg!(args);
+    day1();
+}
 
-    let file_path = &args[1];
-    
+fn header(day: &str){
+    println!("Day {:?} ============", day)
+}
+
+fn day1(){
+    header(&"1");
+    let file_path = "data/1/input.txt";
     println!("In file {}", file_path);
-    let elves = day1::parse(day1::read(file_path.to_owned())).to_owned();
+    let contents = lib::fs_utils::read(file_path.to_owned());
+    let elves = day1::parse(contents).to_owned();
     println!("Elf with most is {:?} ", day1::who_most(&elves));
     println!("Top 3 Elves with most is {:?} ", day1::top_three(&elves));
+}
+
+fn day2(){
+    header(&"2");
+    let file_path = "data/2/input.txt";
 
 }
